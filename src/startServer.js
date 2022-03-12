5b563a76be123db6a60448e245d7dd5aa57cd863
Michel Stefani <michel@gavea.com>
2022-03-12 20:32:19 -0300

    finish project graphql

---
+++ src/startServer.js
@@ -0,0 +1,14 @@
+import { ApolloServer, PubSub } from 'apollo-server'
+import mongoose from 'mongoose';
+
+function startServer({ typeDefs, resolvers }) {
+  mongoose.connect('mongodb+srv', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  const pubsub = new PubSub();
+  const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub } });
+  server.listen().then(({ url }) => console.log(`🔥 Server started at ${url}`))
+}
+
+export default startServer;
